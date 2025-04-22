81a909699bb1f2695feaa254070afa35f4417f6f
Robert Johansson <Wizti@users.noreply.github.com>
2025-04-22 16:10:47 +0200

    added localhost adress on httpclient and set DI service for client

--- FribergRealEstatesClient/Program.cs
+++ FribergRealEstatesClient/Program.cs
@@ -1,3 +1,4 @@
+using FribergRealEstatesClient.Services.Base;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
@@ -13,8 +14,10 @@ namespace FribergRealEstatesClient
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7053") });
+
+            builder.Services.AddScoped<IClient, Client>();
+
             // MudBlazor
             builder.Services.AddMudServices();
             builder.Services.AddMudServices(config =>
