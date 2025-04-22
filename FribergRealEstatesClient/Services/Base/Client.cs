5712588e8209410253691625dcaff4adbc647506
Robert Johansson <Wizti@users.noreply.github.com>
2025-04-22 16:05:00 +0200

    added partial Client class

---
+++ FribergRealEstatesClient/Services/Base/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace FribergRealEstatesClient.Services.Base
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient
+        {
+            get {  return _httpClient; }
+        }
+    }
+}
