7ca6fe7cf1158683cf488b62bc5122c1d16e5fe1
Oscar Hasselrot <o.hasselrot@gmail.com>
2025-05-08 13:29:46 +0200

    Merge

---
+++ FribergRealEstatesClient/Services/CommunService.cs
@@ -0,0 +1,20 @@
+﻿using Blazored.LocalStorage;
+using FribergRealEstatesClient.Services.Base;
+
+namespace FribergRealEstatesClient.Services
+{
+    public class CommunService : BaseHttpService ,ICommunService
+    {
+        private readonly IClient client;
+
+        public CommunService(IClient client, ILocalStorageService localStorage) : base(client, localStorage)
+        {
+            this.client = client;
+        }
+
+        public async Task<List<CommmunDto>> GetAllCommunsAsync()
+        {
+           return client.C
+        }
+    }
+}
---
+++ FribergRealEstatesClient/Services/ICommunService.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FribergRealEstatesClient.Services
+{
+    public interface ICommunService
+    {
+        Task<List<CommmunDto>> GetAllCommunsAsync();
+    }
+}
