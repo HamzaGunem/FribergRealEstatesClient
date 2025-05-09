f93f25f87e2081286090e2130aeb53df86c10ff5
SamAndrs <sam_andrs@yahoo.se>
2025-05-09 11:41:09 +0200

    CreateResidence page updates

---
+++ FribergRealEstatesClient/Services/ResidenceService.cs
@@ -0,0 +1,31 @@
+﻿using Blazored.LocalStorage;
+using FribergRealEstatesClient.Services.Base;
+
+namespace FribergRealEstatesClient.Services
+{
+    // made by Samuel
+    public class ResidenceService : BaseHttpService, IResidenceService
+    {
+        private readonly IClient _client;
+
+        public ResidenceService(IClient client, ILocalStorageService localStorage) : base(client, localStorage)
+        {
+            _client = client;
+        }
+
+        // by Samuel
+        // kräver SuperAdmin role
+        public async Task CreateResidence(CreateResidenceDto dto)
+        {
+            try
+            {
+                await _client.ResidencePOSTAsync(dto);
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine($"Fel vid skapande av försäljningsobjekt: {ex.Message}");
+            }
+            
+        }
+    }
+}
